589f08bea1d3904f630beda90ebd4e85d2778a4e
Nicolás Peralta <nicolas@h1lab.com>
2012-11-09 19:48:48 -0300

    Clase de geolocalización

--- Resources/lib/Geo.js
+++ Resources/lib/Geo.js
@@ -1,113 +1,81 @@
 
-Titanium.Geolocation.accuracy = Titanium.Geolocation.ACCURACY_BEST;
-Titanium.Geolocation.distanceFilter = 4;
+var self = this;
 
-var locationAdded = false;
+function Geo() {
 
+	Titanium.Geolocation.accuracy = Titanium.Geolocation.ACCURACY_BEST;
+	Titanium.Geolocation.distanceFilter = 4;
 
-	
-if (Titanium.Platform.name == 'iPhone OS') {
-	// a partir de la version 3.2 necesitas especificar un proposito
-	Ti.Geolocation.purpose = "GPS user coordinates";
+	self.locationAdded = false;
+
+	// default values
+	self.latitude  = -34.569281;
+	self.longitude = -58.468939;
+	self.altitude  = null;
+	self.heading   = null;
+	self.accuracy  = null;
+	self.speed     = null;
+	self.timestamp = null;
+	self.altitudeAccuracy = null;
+		
+	if (Titanium.Platform.name == 'iPhone OS') {
+		// a partir de la version 3.2 necesitas especificar un proposito
+		Ti.Geolocation.purpose = "GPS user coordinates";
+	}
+
 }
 
 // Solo para android
-function turnOffGPSListeners(currAct) {	
-	//  as the destroy handler will remove the listener, only set the pause handler to remove if you need battery savings
-	/*
-	currAct.addEventListener('pause', function(e) {
-		Ti.API.info("pause event received");
-		if (locationAdded) {
-			Ti.API.info("removing location callback on pause");
-			Titanium.Geolocation.removeEventListener('location', locationCallback);
-			locationAdded = false;
-		}
-	});
-	*/
+Geo.prototype.turnOffGPSListeners = function(currAct) {	
 	currAct.addEventListener('destroy', function(e) {
-		if (locationAdded) {
-			Ti.API.info("removing location callback on destroy");
+		if (movies.locationAdded) {
 			Titanium.Geolocation.removeEventListener('location', locationCallback);
-			locationAdded = false;
+			movies.locationAdded = false;
 		}
 	});
-	/*
-	currAct.addEventListener('resume', function(e) {
-		Ti.API.info("resume event received");
-		if (!locationAdded) {
-			Ti.API.info("adding location callback on resume");
-			Titanium.Geolocation.addEventListener('location', locationCallback);
-			locationAdded = true;
-		}
-	});
-	*/
 }
 
 
-// geolocation listeners
 
-Titanium.Geolocation.getCurrentPosition(function(e) {
-    if (e.error) {
-        return;
-    }
-	
-	longitude = e.coords.longitude;
-	latitude = e.coords.latitude;
-	altitude = e.coords.altitude;
-	heading = e.coords.heading;
-	accuracy = e.coords.accuracy;
-	speed = e.coords.speed;
-	timestamp = e.coords.timestamp;
-	altitudeAccuracy = e.coords.altitudeAccuracy;
-	
-	//Ti.API.info('getCurrent ' + latitude + ' ' + longitude  );
-	
-	locationAdded = true;
-	
-	whereAmI(latitude, longitude);
-	updateMap(latitude, longitude);
-	
-});
+Geo.prototype.getPosition = function(_callback) {
 
-
-var locationCallback = function(e) {
-	if (e.error) {
-		// manage the error
-		return;
-	}
-
-	longitude = e.coords.longitude;
-	latitude = e.coords.latitude;
 	
-	altitude = e.coords.altitude;
-	heading = e.coords.heading;
-	accuracy = e.coords.accuracy;
-	speed = e.coords.speed;
-	timestamp = e.coords.timestamp;
-	altitudeAccuracy = e.coords.altitudeAccuracy;
-
-    // again we use the gathered data
-	// var win = Titanium.UI.currentWindow;
-
-	//Ti.API.info('location ' + latitude + ' ' + longitude + ' ' + winEstrenos.title);
-	
-	locationAdded = true;
-	
-	whereAmI(latitude, longitude);
+	// geolocation listeners, el getCurrentPostition se dispara una sola vez
+	Titanium.Geolocation.getCurrentPosition(function(e) {
+	    if (e.error) {
+	        alert('Se a producido un error en el GPS');
+	        return;
+	    }
 		
-};
+		self.longitude = e.coords.longitude;
+		self.latitude = e.coords.latitude;
+		self.altitude = e.coords.altitude;
+		self.heading = e.coords.heading;
+		self.accuracy = e.coords.accuracy;
+		self.speed = e.coords.speed;
+		self.timestamp = e.coords.timestamp;
+		self.altitudeAccuracy = e.coords.altitudeAccuracy;
+		
+		self.locationAdded = true;
 
-Titanium.Geolocation.addEventListener('location', locationCallback) ;
+		if (_callback) _callback(e.coords);
 
+		
+	});
 
-function whereAmI(latitude, longitude) {
+	// location se dispara cada vez que el gps actualiza su posicion
+	Titanium.Geolocation.addEventListener('location', locationCallback) ;
+
+}
+
+Geo.prototype.whereAmI = function(latitude, longitude) {
 	
 	// Busco la direccion
 
 	var xhr = Ti.Network.createHTTPClient();
 	xhr.setTimeout(60000);
-	xhr.open('GET',  WSHOST + '/cines/whereami/' + latitude + '/' + longitude );
-	//Ti.API.info(WSHOST + '/cines/whereami/' + latitude + '/' + longitude );
+	xhr.open('GET',  WSHOST + '/cines/whereami/' + movies.latitude + '/' + movies.longitude );
+
 	xhr.send();
 	
 	xhr.onerror = function(e) {
@@ -116,9 +84,7 @@ function whereAmI(latitude, longitude) {
 
 	xhr.onload = function() {	
 		var json = eval('('+this.responseText+')');
-		//var json = JSON.parse(this.responseData);
-		//Ti.API.info(json);
-		
+			
 		//whereAmIText.text = 'Tu ubicación es ' + json.direccion;
 		whereAmITextCartelera.text = 'Tu ubicación es ' + json.direccion;
 		//whereAmITextCines.text = 'Tu ubicación es ' + json.direccion;
@@ -126,5 +92,33 @@ function whereAmI(latitude, longitude) {
 	
 }
 
-function updateMap(latitude, longitude) {
+Geo.prototype.getLatitude = function (){
+	return self.latitude;
 }
+
+Geo.prototype.getLongitude = function (){
+	return self.longitude;
+}
+
+// private
+var locationCallback = function(e) {
+	if (e.error) {
+		// manage the error
+		return;
+	}
+
+	self.longitude = e.coords.longitude;
+	self.latitude = e.coords.latitude;	
+	self.altitude = e.coords.altitude;
+	self.heading = e.coords.heading;
+	self.accuracy = e.coords.accuracy;
+	self.speed = e.coords.speed;
+	self.timestamp = e.coords.timestamp;
+	self.altitudeAccuracy = e.coords.altitudeAccuracy;
+
+	self.locationAdded = true;	
+		
+};
+
+// public interface
+module.exports = Geo;
